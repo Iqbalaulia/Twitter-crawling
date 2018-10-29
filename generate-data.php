0e62f298c13520995c03f3459eb6c68b16f0f2d9
aulia <miqbalar16@gmail.com>
2018-10-29 23:45:57 +0700

    Penambahan data-timeline dan generate-data

--- generate-data.php
+++ generate-data.php
@@ -7,16 +7,16 @@ $consumer_secret ="Nmp7Li9gdiI5BthAvuWcrEpMxwTV6R3m06AebK9jce6cD2ZZG2";
 $acces_token ="829150783-TAzpWDHnjDG5MvJiqFh2foQhYa5jnk70Zq4PBBIw";
 $acces_token_screet ="0RdF5PK2bTtHfIOYZ0QK2shTml7Gq9P99pKI71xkpHVRo";
 
-@$keyword = $_POST['keyword'];
+// @$keyword = $_POST['keyword'];
 
 $twitter = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
 
-$tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=stikom&result_type=recent&count=10');
+$tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=stikom&result_type=recent&count=1000');
 echo "<pre>";
 echo json_encode($tweets);
 //print_r($tweets);
 echo "</pre>";
-$result = array();
+// $result = array();
 $status = $tweets->statuses;
 
 
@@ -34,9 +34,20 @@ $status = $tweets->statuses;
     $query = mysqli_query($db, $sql);
 
  }
-
-
   //echo json_encode(array("result"=>$result));
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
